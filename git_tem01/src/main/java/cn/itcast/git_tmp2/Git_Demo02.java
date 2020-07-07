451e792b912383774391d4055f0917d3d9d09e5f
jiaming <jiaming@126.com>
2020-07-07 10:54:03 +0800

    我增加一条语句

--- git_tem01/src/main/java/cn/itcast/git_tmp2/Git_Demo02.java
+++ git_tem01/src/main/java/cn/itcast/git_tmp2/Git_Demo02.java
@@ -5,5 +5,6 @@ import java.sql.SQLOutput;
 public class Git_Demo02 {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("修改一下");
     }
 }
